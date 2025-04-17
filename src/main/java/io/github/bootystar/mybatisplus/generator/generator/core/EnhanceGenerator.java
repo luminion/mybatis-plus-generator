eed9e1d22e476d2ddba036c2a2f7da7dd7b800de
bootystar <bootystar@qq.com>
2025-04-17 14:17:30 +0800

    精简方法

--- src/main/java/io/github/bootystar/mybatisplus/generator/generator/core/EnhanceGenerator.java
+++ src/main/java/io/github/bootystar/mybatisplus/generator/generator/core/EnhanceGenerator.java
@@ -1,10 +1,6 @@
 package io.github.bootystar.mybatisplus.generator.generator.core;
 
 import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.builder.Controller;
-import com.baomidou.mybatisplus.generator.config.builder.Entity;
-import com.baomidou.mybatisplus.generator.config.builder.Mapper;
-import com.baomidou.mybatisplus.generator.config.builder.Service;
 import io.github.bootystar.mybatisplus.generator.config.builder.BaseBuilder;
 
 import java.util.function.Consumer;
@@ -17,25 +13,25 @@ public interface EnhanceGenerator<B extends BaseBuilder<B>> {
 
     void execute(String... tableNames);
 
-    EnhanceGenerator<B> dataSource(Consumer<DataSourceConfig.Builder> consumer);
+    EnhanceGenerator<B> dataSourceConfig(Consumer<DataSourceConfig.Builder> consumer);
 
-    EnhanceGenerator<B> global(Consumer<GlobalConfig.Builder> consumer);
+    EnhanceGenerator<B> globalConfig(Consumer<GlobalConfig.Builder> consumer);
 
-    EnhanceGenerator<B> pkg(Consumer<PackageConfig.Builder> consumer);
+    EnhanceGenerator<B> packageConfig(Consumer<PackageConfig.Builder> consumer);
 
-    EnhanceGenerator<B> strategy(Consumer<StrategyConfig.Builder> consumer);
+    EnhanceGenerator<B> strategyConfig(Consumer<StrategyConfig.Builder> consumer);
 
-    EnhanceGenerator<B> entity(Consumer<Entity.Builder> consumer);
+    EnhanceGenerator<B> injectionConfig(Consumer<InjectionConfig.Builder> consumer);
 
-    EnhanceGenerator<B> mapper(Consumer<Mapper.Builder> consumer);
+    EnhanceGenerator<B> customConfig(Consumer<B> consumer);
 
-    EnhanceGenerator<B> service(Consumer<Service.Builder> consumer);
-
-    EnhanceGenerator<B> controller(Consumer<Controller.Builder> consumer);
-
-    EnhanceGenerator<B> injection(Consumer<InjectionConfig.Builder> consumer);
-
-    EnhanceGenerator<B> custom(Consumer<B> consumer);
+//    EnhanceGenerator<B> entity(Consumer<Entity.Builder> consumer);
+//
+//    EnhanceGenerator<B> mapper(Consumer<Mapper.Builder> consumer);
+//
+//    EnhanceGenerator<B> service(Consumer<Service.Builder> consumer);
+//
+//    EnhanceGenerator<B> controller(Consumer<Controller.Builder> consumer);
 
     EnhanceGenerator<B> mapperXmlResource(String path);
 
