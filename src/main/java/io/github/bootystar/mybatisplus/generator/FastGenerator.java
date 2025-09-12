3ac1af2edf09389c4012b7bb4e118c2e475092c1
lo <604647004@qq.com>
2025-09-12 15:29:49 +0800

    initialize()减少table fills默认值

--- src/main/java/io/github/bootystar/mybatisplus/generator/FastGenerator.java
+++ src/main/java/io/github/bootystar/mybatisplus/generator/FastGenerator.java
@@ -100,18 +100,18 @@ public class FastGenerator {
                         .addTableFills(
                                 new Column("create_time", FieldFill.INSERT),
                                 new Column("update_time", FieldFill.INSERT_UPDATE),
-                                new Column("created_time", FieldFill.INSERT),
-                                new Column("updated_time", FieldFill.INSERT_UPDATE),
-                                new Column("create_at", FieldFill.INSERT),
-                                new Column("update_at", FieldFill.INSERT_UPDATE),
-                                new Column("created_at", FieldFill.INSERT),
-                                new Column("updated_at", FieldFill.INSERT_UPDATE),
-                                new Column("creator_id", FieldFill.INSERT),
-                                new Column("updater_id", FieldFill.INSERT_UPDATE),
-                                new Column("create_by", FieldFill.INSERT),
-                                new Column("update_by", FieldFill.INSERT_UPDATE),
-                                new Column("created_by", FieldFill.INSERT),
-                                new Column("updated_by", FieldFill.INSERT_UPDATE)
+//                                new Column("created_time", FieldFill.INSERT),
+//                                new Column("updated_time", FieldFill.INSERT_UPDATE),
+//                                new Column("create_at", FieldFill.INSERT),
+//                                new Column("update_at", FieldFill.INSERT_UPDATE),
+//                                new Column("created_at", FieldFill.INSERT),
+//                                new Column("updated_at", FieldFill.INSERT_UPDATE),
+//                                new Column("creator_id", FieldFill.INSERT),
+//                                new Column("updater_id", FieldFill.INSERT_UPDATE),
+//                                new Column("create_by", FieldFill.INSERT),
+//                                new Column("update_by", FieldFill.INSERT_UPDATE),
+//                                new Column("created_by", FieldFill.INSERT),
+//                                new Column("updated_by", FieldFill.INSERT_UPDATE)
                         ))
                 .model(e -> e
                         .enableQueryDTOExtendsEntity()
